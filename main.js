7b1ee9a05a90137c034b204a8012d77aebccb020
Neil <Neil10241126@gmail.com>
2023-08-19 11:00:31 +0800

    新增AOS視差滾動

--- main.js
+++ main.js
@@ -2,4 +2,8 @@ import './assets/icofont/icofont.min.css';
 import './assets/scss/all.scss';
 import 'bootstrap/dist/js/bootstrap.min.js';
 
-console.log("Hello world!");
+// AOS 套件
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// 初始化
+AOS.init();
